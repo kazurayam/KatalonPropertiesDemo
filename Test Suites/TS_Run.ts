0deddacd8eb906116740f54595f0b9bfdd514569
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-03-30 17:24:13 +0900

    add settings>Execution>Remote for BrowserStack

--- Test Suites/TS_Run.ts
+++ Test Suites/TS_Run.ts
@@ -4,7 +4,7 @@
    <name>TS_Run</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-03-27T10:05:24</lastRun>
+   <lastRun>2018-03-27T15:48:20</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
